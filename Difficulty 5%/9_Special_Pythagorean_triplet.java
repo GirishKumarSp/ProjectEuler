415bd5411e671d0fb806573e44c8db37a3b9f026
Girish Kumar <52626613+GirishKumarSp@users.noreply.github.com>
2021-12-11 00:23:47 +0530

    Create 9_Special_Pythagorean_triplet.java

---
+++ Difficulty 5%/9_Special_Pythagorean_triplet.java
@@ -0,0 +1,34 @@
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//     a² + b² = c²
+// For example, 3² + 4² = 9 + 16 = 25 = 5².
+
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+int sum=1000;
+for(int i=1;i<sum/3;i++){
+  for(int j=i+1;j<sum/2;j++){
+    int k =sum-i-j;
+    if (i*i + j*j == k*k) {
+      if(i+j+k==1000){
+        System.out.printf("%d²+%d²=%d² \n",i, j, k );
+        System.out.printf("%d*%d*%d=%d",i, j, k,i*j*k);
+      }
+    }
+  }
+}
+
+//2nd method
+
+// for(int i=1;i<1000;i++){
+//     for(int j=i+1;j<1000;j++){
+//         for(int k=1;k<1000;k++) {
+//              if (i*i + j*j == k*k) {
+//                  if(i+j+k==1000){
+//                      System.out.printf("%d²+%d²=%d²",i, j, k);
+//                      System.out.println();
+//                      System.out.printf("%d*%d*%d=%d",i, j, k,i*j*k);
+//                   }
+//              }
+//         }
+//     }
+// }
